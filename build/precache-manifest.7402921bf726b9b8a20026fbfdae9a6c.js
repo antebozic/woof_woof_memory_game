2a3a7900e502b45d43b23a6cf7bd1e22c9ab00b1
Ante Bozic <ante@Antes-MacBook-Pro.local>
2019-01-07 00:02:51 +0100

    warning message styled

--- build/precache-manifest.7402921bf726b9b8a20026fbfdae9a6c.js
+++ build/precache-manifest.7402921bf726b9b8a20026fbfdae9a6c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/woof/static/js/runtime~main.d38e75a6.js"
   },
   {
-    "revision": "6003286d3f4ac40a501f",
-    "url": "/woof/static/js/main.6003286d.chunk.js"
+    "revision": "1bc3ecccce7ef1c523d8",
+    "url": "/woof/static/js/main.1bc3eccc.chunk.js"
   },
   {
     "revision": "e49c2389bb712159f258",
     "url": "/woof/static/js/1.e49c2389.chunk.js"
   },
   {
-    "revision": "6003286d3f4ac40a501f",
-    "url": "/woof/static/css/main.f19b7463.chunk.css"
+    "revision": "1bc3ecccce7ef1c523d8",
+    "url": "/woof/static/css/main.2f48678b.chunk.css"
   },
   {
-    "revision": "ace118c55dff4cf0004ff7b9ceeadaf1",
+    "revision": "4940ced3bbde9c9e117784eae2c995ce",
     "url": "/woof/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/woof/precache-manifest.6e903213a54044ecc16678e355b8ef6b.js"
+  "/woof/precache-manifest.7402921bf726b9b8a20026fbfdae9a6c.js"
 );
 
 workbox.clientsClaim();
